094ecaf6db6520cb0b97b74b98d37382f11b7427
Claire <jycchoi@bu.edu>
2021-09-24 13:37:51 -0400

    Draw Market

--- 01-python/leisure/market.py
+++ 01-python/leisure/market.py
@@ -1,3 +1,7 @@
 def draw_market():
-    print("market not found")
+    print("Trader Joe's")
+    print("Bread: White, Rye, Wheat, Sourdough")
+    print("Vegetables: Spinach, Bell Peppers, Cucumbers, Carrots")
+    print("Spices: Red Pepper Flakes, Salt, Pepper, Everything But the Bagel Seasoning")
+    print("Frozen treats: Tiramisu, Ice Cream, Mochi Ice Cream")
     return
