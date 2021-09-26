cd61e03b49611d9960d4c85c7cd7e7dc8a9d3b56
Claire <jycchoi@bu.edu>
2021-09-26 14:51:51 -0400

    made changes

--- 01-python/leisure/market.py
+++ 01-python/leisure/market.py
@@ -1,7 +1,7 @@
 def draw_market():
-    print("Trader Joe's")
-    print("Bread: White, Rye, Wheat, Sourdough")
-    print("Vegetables: Spinach, Bell Peppers, Cucumbers, Carrots")
-    print("Spices: Red Pepper Flakes, Salt, Pepper, Everything But the Bagel Seasoning")
-    print("Frozen treats: Tiramisu, Ice Cream, Mochi Ice Cream")
+    print("Trader Joe's\n")
+    print("Bread: White, Rye, Wheat, Sourdough\n")
+    print("Vegetables: Spinach, Bell Peppers, Cucumbers, Carrots\n")
+    print("Spices: Red Pepper Flakes, Salt, Pepper, Everything But the Bagel Seasoning\n")
+    print("Frozen treats: Tiramisu, Ice Cream, Mochi Ice Cream\n")
     return
