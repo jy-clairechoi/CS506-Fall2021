21a43c02d5ddca24dc1722320f3c3eb8adba3229
Claire <jycchoi@bu.edu>
2021-09-30 16:14:44 -0400

    read.py

--- 02-library/cs506/dbscan.py
+++ 02-library/cs506/dbscan.py
@@ -1,9 +1,29 @@
+from .sim import euclidean_dist
+
 class DBC():
 
     def __init__(self, dataset, min_pts, epsilon):
         self.dataset = dataset
         self.min_pts = min_pts
         self.epsilon = epsilon
+
+    def _get_epsilon_neighborhood(self, idx):
+        neighborhood = []
+        for i, y in enumerate(self.dataset):
+            if i == idx:
+                continue
+            if euclidean_dist(self.dataset[idx], y) <= self.epsilon:
+                neighborhood.append(i)
+        return neighborhood
+
+    def _make_bfs_assignments(self, queue, assignment, assignments):
+        while queue:
+            nextP = queue.pop(0)
+            if assignments[nextP] != 0:
+                continue
+            assignments[nextP] = assignment
+
+        return assignments
 
 
     def dbscan(self):
@@ -12,5 +32,14 @@ class DBC():
             assignment should match the index of the data point
             in the dataset.
         """
-        
-        raise NotImplementedError
+        assignments = [0 for _ in range(len(self.dataset()))]
+        assignment = 1
+        for i, x in enumerate(self.dataset):
+            # if 
+            neighborhood = self._get_epsilon_neighborhood(x)
+            if len(neighborhood) >= self.min_pts:
+                # explore x neighborhood for core points and give them the same assignment
+                assignments[i] = assignment
+                assignments = self._make_bfs_assignments(neighborhood, assignment, assignments)
+
+        return assignments
--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,18 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    res = []
+    csv_file = open(csv_file_path,'r')
+    for line in csv_file.readlines():
+        row = []
+        values = line.strip('\n').split(',')
+        for val in values:
+            try:
+                converted = int(val)
+            except ValueError:
+                converted = val.strip("\"")
+            row.append(converted)
+        res.append(row)
+    print(res)
+    return res
+
